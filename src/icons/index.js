b8958366921f5b374b1daad41af0a4efdc74e0b1
Renny Galindez <rfgalindez@gmail.com>
2020-08-30 00:11:51 -0500

    Install and config modules of FontAwesome (Icons)

---
+++ src/icons/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faHeart as solidHeart,
+  faEye as solidEye,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faHeart as regularHeart,
+  faEye as regularEye,
+  faChartBar,
+} from '@fortawesome/free-regular-svg-icons';
+
+library.add([solidHeart, regularHeart, solidEye, regularEye, faChartBar]);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
