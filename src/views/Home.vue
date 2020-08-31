719a45abe47b37a3ce87430d97a60c0a5e1c0edf
Renny Galindez <rfgalindez@gmail.com>
2020-08-31 15:50:20 -0500

    Define some styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="home">
-    Wellcome to Pokemon Shop
+    <h1>Wellcome to Pokemon Shop</h1>
     <ThePokemonsList />
   </div>
 </template>
 
 <script>
-import ThePokemonsList from '@/components/ThePokemonsList';
+import ThePokemonsList from "@/components/ThePokemonsList";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: { ThePokemonsList },
 };
 </script>
+<style>
+h1 {
+  padding: 1rem;
+  text-align: center;
+  font-size: 0.8rem;
+}
+@media (min-width: 640px) {
+  h1 {
+    font-size: 2rem;
+    padding: 2rem;
+  }
+}
+</style>
