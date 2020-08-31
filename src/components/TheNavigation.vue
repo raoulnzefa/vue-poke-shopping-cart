eb414fc7d5968b3b5477ccf9b8d71d4ca6074fcb
Renny Galindez <rfgalindez@gmail.com>
2020-08-31 15:49:37 -0500

    The navigation component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navigation">
+    <ul class="navigation_ul">
+      <li>PokeShop</li>
+      <li>
+        <font-awesome-icon @click="changeShow" :icon="['fas','shopping-cart']" size="2x" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: { ...mapActions("cart", { changeShow: "changeShow" }) },
+};
+</script>
+
+<style scoped>
+.navigation {
+  padding: 2rem;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  background: #fff;
+}
+.navigation_ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+}
+</style>
